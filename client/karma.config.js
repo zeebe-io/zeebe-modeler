3f310c13ee2d4fc5cdc706df26f27247df5dc7fc
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-01-15 09:01:06 +0100

    fix(karma): remove unrelated files

--- client/karma.config.js
+++ client/karma.config.js
@@ -118,7 +118,7 @@ module.exports = function(karma) {
         ],
         alias: {
           'bpmn-js/lib/Modeler': 'test/mocks/bpmn-js/Modeler',
-          './CodeMirror': 'test/mocks/codemirror/Codemirror'
+          './CodeMirror': 'test/mocks/code-mirror/Codemirror'
         }
       }
     }
