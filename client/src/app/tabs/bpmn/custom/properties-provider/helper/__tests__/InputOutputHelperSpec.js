f5689ef339b73baea575aa85899644e1a41ab9e2
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-10-18 09:01:21 +0200

    test(client/properties-provider): test coverage for input output helper

---
+++ client/src/app/tabs/bpmn/custom/properties-provider/helper/__tests__/InputOutputHelperSpec.js
@@ -0,0 +1,178 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+import {
+  bootstrapModeler,
+  inject
+} from 'bpmn-js/test/helper';
+
+import coreModule from 'bpmn-js/lib/core';
+
+import modelingModule from 'bpmn-js/lib/features/modeling';
+
+import {
+  areInputParametersSupported,
+  areOutputParametersSupported
+} from '../InputOutputHelper';
+
+describe('customs - input output helper', function() {
+
+  const diagramXML = require('./diagram.bpmn');
+
+  const testModules = [
+    coreModule,
+    modelingModule
+  ];
+
+  beforeEach(bootstrapModeler(diagramXML, {
+    modules: testModules,
+  }));
+
+  let createElement;
+
+  beforeEach(inject(function(elementFactory) {
+    createElement = (type, options) => {
+      const element = elementFactory.createShape({
+        type,
+        ...options
+      });
+
+      return element;
+    };
+  }));
+
+
+  describe('#areInputParametersSupported', function() {
+
+    it('should support ServiceTask', function() {
+
+      // given
+      const serviceTask = createElement('bpmn:ServiceTask');
+
+      // when
+      const supported = areInputParametersSupported(serviceTask);
+
+      // then
+      expect(supported).to.be.true;
+    });
+
+
+    it('should support SubProcess', function() {
+
+      // given
+      const subProcess = createElement('bpmn:SubProcess');
+
+      // when
+      const supported = areInputParametersSupported(subProcess);
+
+      // then
+      expect(supported).to.be.true;
+    });
+
+
+    it('should support CallActivity', function() {
+
+      // given
+      const callActivity = createElement('bpmn:CallActivity');
+
+      // when
+      const supported = areInputParametersSupported(callActivity);
+
+      // then
+      expect(supported).to.be.true;
+    });
+
+
+    it('should NOT support ReceiveTask', function() {
+
+      // given
+      const receiveTask = createElement('bpmn:ReceiveTask');
+
+      // when
+      const supported = areInputParametersSupported(receiveTask);
+
+      // then
+      expect(supported).to.be.false;
+    });
+
+  });
+
+
+  describe('#areOutputParametersSupported', function() {
+
+    it('should support ServiceTask', function() {
+
+      // given
+      const serviceTask = createElement('bpmn:ServiceTask');
+
+      // when
+      const supported = areOutputParametersSupported(serviceTask);
+
+      // then
+      expect(supported).to.be.true;
+    });
+
+
+    it('should support SubProcess', function() {
+
+      // given
+      const subProcess = createElement('bpmn:SubProcess');
+
+      // when
+      const supported = areOutputParametersSupported(subProcess);
+
+      // then
+      expect(supported).to.be.true;
+    });
+
+
+    it('should support CallActivity', function() {
+
+      // given
+      const callActivity = createElement('bpmn:CallActivity');
+
+      // when
+      const supported = areOutputParametersSupported(callActivity);
+
+      // then
+      expect(supported).to.be.true;
+    });
+
+
+    it('should support ReceiveTask', function() {
+
+      // given
+      const receiveTask = createElement('bpmn:ReceiveTask');
+
+      // when
+      const supported = areOutputParametersSupported(receiveTask);
+
+      // then
+      expect(supported).to.be.true;
+    });
+
+
+    it('should support MessageEvent', function() {
+
+      // given
+      const messageEvent = createElement('bpmn:IntermediateCatchEvent', {
+        eventDefinitionType: 'bpmn:MessageEventDefinition'
+      });
+
+      // when
+      const supported = areOutputParametersSupported(messageEvent);
+
+      // then
+      expect(!!supported).to.be.true;
+    });
+
+  });
+
+});
