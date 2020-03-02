89e6b0b3da24dc308552928d0d7a6001abe5ffd7
Oguz <eroglu.oguz93@gmail.com>
2020-03-02 13:06:25 +0100

    feat(client): zeebe-plugin

---
+++ client/src/plugins/zeebe-plugin/ZeebePlugin.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+import React, { PureComponent } from 'react';
+
+import { forEach, omit } from 'min-dash';
+
+/**
+ * A parent plugin for these two plugins:
+ * a) DeploymentPlugin
+ * b) StartInstancePlugin
+ */
+export default class ZeebePlugin extends PureComponent {
+
+  constructor(props) {
+    super(props);
+
+    this.subscriptions = {};
+  }
+
+  subscribeToMessaging = (pluginName, callback) => {
+    this.subscriptions[pluginName] = callback;
+  }
+
+  unsubscribeFromMessaging = (pluginName) => {
+    this.subscriptions = omit(this.subscriptions, pluginName);
+  }
+
+  broadcastMessage = (message, body) => {
+    forEach(this.subscriptions, (callback) => {
+      callback(message, body);
+    });
+  }
+
+  render() {
+    return <React.Fragment>
+
+    </React.Fragment>;
+  }
+}
