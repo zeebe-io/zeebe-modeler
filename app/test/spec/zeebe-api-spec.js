e829713d37fe2d8bece6563e291ccf9fcf5478c2
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-04-24 13:13:35 +0200

    feat(zeebe-api): refactor APIs
    * Expect dependencies passed to constructor.
    * Expect endpoint configuration for all methods.
    * Re-use Zeebe Client instance.
    
    Closes #215
    
    Co-authored by: Oguz <eroglu.oguz93@gmail.com>

---
+++ app/test/spec/zeebe-api-spec.js
@@ -0,0 +1,301 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+'use strict';
+
+const sinon = require('sinon');
+
+const ZeebeAPI = require('../../lib/zeebe-api');
+
+
+describe('ZeebeAPI', function() {
+
+  describe('#checkConnectivity', function() {
+
+    it('should set success=true for correct check', async () => {
+
+      // given
+      const zbAPI = mockZB();
+      const parameters = {
+        endpoint: {
+          type: 'selfHosted',
+          url: 'https://google.com'
+        }
+      };
+
+      // when
+      const result = await zbAPI.checkConnectivity(parameters);
+
+      // then
+      expect(result).to.have.property('success', true);
+    });
+
+
+    it('should set success=false on failure', async () => {
+
+      // given
+      // given
+      const zbAPI = mockZB({
+        ZBClient: function() {
+          return {
+            topology: function() {
+              throw new Error('TEST ERROR.');
+            }
+          };
+        }
+      });
+      const parameters = {
+        endpoint: {
+          type: 'selfHosted',
+          url: 'https://google.com'
+        }
+      };
+
+      // when
+      const result = await zbAPI.checkConnectivity(parameters);
+
+      // then
+      expect(result).to.have.property('success', false);
+    });
+
+    // describe('should return correct error reason on failure');
+  });
+
+  describe('#run', function() {
+
+
+    it('should set success=true for successful instance run', async () => {
+
+      // given
+      const zbAPI = mockZB();
+      const parameters = {
+        endpoint: {
+          type: 'selfHosted',
+          url: 'testURL'
+        }
+      };
+
+      // when
+      const result = await zbAPI.run(parameters);
+
+      // then
+      expect(result).to.exist;
+      expect(result).to.have.property('success', true);
+      expect(result).to.have.property('response');
+    });
+
+
+    it('should set success=false on failure', async () => {
+
+      // given
+      const zbAPI = mockZB({
+        ZBClient: function() {
+          return {
+            createWorkflowInstance: function() {
+              throw new Error('TEST ERROR.');
+            }
+          };
+        }
+      });
+      const parameters = {
+        endpoint: {
+          type: 'selfHosted',
+          url: 'testURL'
+        }
+      };
+
+      // when
+      const result = await zbAPI.run(parameters);
+
+      // then
+      expect(result).to.exist;
+      expect(result).to.have.property('success', false);
+      expect(result).to.have.property('response');
+      expect(result.response.message).to.be.eql('TEST ERROR.');
+    });
+  });
+
+  describe('#deploy', function() {
+
+
+    it('should set success=true for successful deployment', async () => {
+
+      // given
+      const zbAPI = mockZB();
+      const parameters = {
+        endpoint: {
+          type: 'selfHosted',
+          url: 'testURL'
+        }
+      };
+
+      // when
+      const result = await zbAPI.deploy(parameters);
+
+      // then
+      expect(result).to.exist;
+      expect(result).to.have.property('success', true);
+      expect(result).to.have.property('response');
+    });
+
+
+    it('should set success=false for failure', async () => {
+
+      // given
+      const error = new Error('test');
+      const zbAPI = mockZB({
+        ZBClient: function() {
+          return {
+            deployWorkflow: function() {
+              throw error;
+            }
+          };
+        }
+      });
+      const parameters = {
+        endpoint: {
+          type: 'selfHosted',
+          url: 'testURL'
+        }
+      };
+
+      // when
+      const result = await zbAPI.deploy(parameters);
+
+      // then
+      expect(result).to.exist;
+      expect(result).to.have.property('success', false);
+      expect(result).to.have.property('response', error);
+    });
+
+
+    it('should reuse the client instance if config is the same', async () => {
+
+      // given
+      let firstInstance;
+      const zbAPI = mockZB();
+      const parameters = {
+        endpoint: {
+          type: 'selfHosted',
+          url: 'testURL'
+        }
+      };
+
+      // when
+      await zbAPI.deploy(parameters);
+      firstInstance = zbAPI.zbClientInstanceCache;
+
+      await zbAPI.deploy(parameters);
+
+      // then
+      const isSameObject = zbAPI.zbClientInstanceCache === firstInstance;
+
+      expect(firstInstance).to.exist;
+      expect(zbAPI.zbClientInstanceCache).to.exist;
+      expect(isSameObject).to.be.true;
+    });
+
+
+    it('should create new client instance if config is different', async () => {
+
+      // given
+      let firstInstance;
+      const closeSpy = sinon.spy();
+      const zbAPI = mockZB({
+        ZBClient: function() {
+          return {
+            deployWorkflow: noop,
+            close: closeSpy
+          };
+        }
+      });
+      const parameters = {
+        endpoint: {
+          type: 'selfHosted',
+          url: 'testURL'
+        }
+      };
+
+      // when
+      await zbAPI.deploy(parameters);
+      firstInstance = zbAPI.zbClientInstanceCache;
+
+      await zbAPI.deploy({
+        ...parameters,
+        endpoint: {
+          type: 'oauth',
+          url: 'testURL'
+        }
+      });
+
+      // then
+      const isSameObject = zbAPI.zbClientInstanceCache === firstInstance;
+
+      expect(firstInstance).to.exist;
+      expect(zbAPI.zbClientInstanceCache).to.exist;
+      expect(isSameObject).to.be.false;
+      expect(closeSpy).to.have.been.calledOnce;
+    });
+  });
+
+
+  describe('create client', () => {
+
+    it('should create client with correct url', async () => {
+
+      // given
+      let usedConfig;
+      const zbAPI = mockZB({
+        ZBClient: function(...args) {
+          usedConfig = args;
+
+          return {
+            deployWorkflow: noop
+          };
+        }
+      });
+      const parameters = {
+        endpoint: {
+          type: 'selfHosted',
+          url: 'https://camunda.com'
+        }
+      };
+
+      // when
+      await zbAPI.deploy(parameters);
+
+      // then
+      expect(usedConfig[0]).to.eql('https://camunda.com');
+    });
+  });
+});
+
+function mockZB(parameters = {}) {
+
+
+  const fs = {
+    readFileSync: noop
+  };
+
+  const ZB = {
+    ZBClient: parameters.ZBClient || function() {
+      return {
+        topology: noop,
+        deployWorkflow: noop,
+        createWorkflowInstance: noop,
+        close: noop
+      };
+    }
+  };
+
+  return new ZeebeAPI(fs, ZB);
+}
+
+function noop() {}
