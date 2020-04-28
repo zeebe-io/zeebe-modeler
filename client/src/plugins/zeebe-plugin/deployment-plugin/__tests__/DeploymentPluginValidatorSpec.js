afac872ab10b0d0623ce776f73400ddecc2ccaaa
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-04-28 15:35:03 +0200

    test(zeebe-plugin): add connection checker basic tests

--- client/src/plugins/zeebe-plugin/deployment-plugin/__tests__/DeploymentPluginValidatorSpec.js
+++ client/src/plugins/zeebe-plugin/deployment-plugin/__tests__/DeploymentPluginValidatorSpec.js
@@ -8,6 +8,8 @@
  * except in compliance with the MIT License.
  */
 
+/* global sinon */
+
 import DeploymentPluginValidator from '../DeploymentPluginValidator';
 
 import {
@@ -98,7 +100,7 @@ describe('<DeploymentPluginValidator>', () => {
     });
 
 
-    it('should validate config', () => {
+    it('should validate config (camunda cloud)', () => {
 
       // given
       const config = {
@@ -112,9 +114,212 @@ describe('<DeploymentPluginValidator>', () => {
           camundaCloudClusterId: 'test'
         }
       };
+      const wrongConfig = {
+        deployment: {
+          name: 'name'
+        },
+        endpoint: {
+          targetType: 'camundaCloud',
+          camundaCloudClientId: 'test'
+        }
+      };
 
       // then
       expect(Object.keys(validator.validateConfig(config))).to.have.lengthOf(0);
+      expect(Object.keys(validator.validateConfig(wrongConfig))).to.have.lengthOf(2);
+    });
+
+
+    it('should validate config (self hosted none auth)', () => {
+
+      // given
+      const config = {
+        deployment: {
+          name: 'name'
+        },
+        endpoint: {
+          targetType: 'selfHosted',
+          authType: 'none',
+          contactPoint: 'https://camunda.com'
+        }
+      };
+
+      const wrongConfig = {
+        deployment: {},
+        endpoint: {
+          targetType: 'selfHosted',
+          authType: 'none',
+          contactPoint: 'https://camunda.com'
+        }
+      };
+
+      // then
+      expect(Object.keys(validator.validateConfig(config))).to.have.lengthOf(0);
+      expect(Object.keys(validator.validateConfig(wrongConfig))).to.have.lengthOf(1);
+    });
+
+
+    it('should validate config (self hosted oauth)', () => {
+
+      // given
+      const config = {
+        deployment: {
+          name: 'name'
+        },
+        endpoint: {
+          targetType: 'selfHosted',
+          authType: 'oauth',
+          contactPoint: 'https://camunda.com',
+          oauthURL: 'https://camunda.com',
+          audience: 'bearer',
+          clientId: 'test',
+          clientSecret: 'test'
+        }
+      };
+
+      const wrongConfig = {
+        deployment: {
+          name: 'name'
+        },
+        endpoint: {
+          targetType: 'selfHosted',
+          authType: 'oauth',
+          contactPoint: 'https://camunda.com',
+          oauthURL: 'https://camunda.com'
+        }
+      };
+
+      // then
+      expect(Object.keys(validator.validateConfig(config))).to.have.lengthOf(0);
+      expect(Object.keys(validator.validateConfig(wrongConfig))).to.have.lengthOf(3);
     });
   });
+
+
+  describe('<ConnectionChecker>', () => {
+
+    it('should be created', () => {
+
+      // given
+      const connectionChecker = createConnectionChecker();
+
+      // then
+      expect(connectionChecker).to.exist;
+    });
+
+
+    describe('#check', () => {
+
+      it('should work', async () => {
+
+        // given
+        const connectionChecker = createConnectionChecker();
+
+        // when
+        const { connectionResult } = await connectionChecker.check({});
+
+        // then
+        expect(connectionResult).to.eql({ success: true, response: {} });
+      });
+
+
+      it('should return last result if endpoint did not change', async () => {
+
+        // given
+        const spy = sinon.spy(() => Promise.resolve({ success: true, response: {} }));
+        const endpoint = {};
+        const connectionChecker = createConnectionChecker(spy);
+
+        // when
+        await connectionChecker.check(endpoint);
+        await connectionChecker.check(endpoint);
+
+        // then
+        expect(spy).to.have.been.calledOnce;
+      });
+
+
+      it('should check again if endpoint changed', async () => {
+
+        // given
+        const spy = sinon.spy(() => Promise.resolve({ success: true, response: {} }));
+        const endpoint = {};
+        const connectionChecker = createConnectionChecker(spy);
+
+        // when
+        await connectionChecker.check(endpoint);
+        await connectionChecker.check({ url: 'new' });
+
+        // then
+        expect(spy).to.have.been.calledTwice;
+      });
+
+    });
+
+
+    describe('#subscribe', () => {
+
+      it('should work', async () => {
+
+        // given
+        const onStart = sinon.spy();
+        const onComplete = sinon.spy();
+        const connectionChecker = createConnectionChecker();
+        connectionChecker.subscribe({ onStart, onComplete });
+
+        // when
+        const result = await connectionChecker.check({});
+
+        // then
+        expect(onStart).to.have.been.calledOnce;
+        expect(onComplete).to.have.been.calledOnce;
+        expect(onComplete.args).to.eql([ [ result ] ]);
+      });
+
+    });
+
+
+    describe('#unsubscribe', () => {
+
+      it('should work', async () => {
+
+        // given
+        const onStart = sinon.spy();
+        const onComplete = sinon.spy();
+        const connectionChecker = createConnectionChecker();
+        connectionChecker.subscribe({ onStart, onComplete });
+        connectionChecker.unsubscribe();
+
+        // when
+        await connectionChecker.check({});
+
+        // then
+        expect(onStart).not.to.have.been.called;
+        expect(onComplete).not.to.have.been.called;
+      });
+
+    });
+
+  });
 });
+
+
+
+// helper
+function createConnectionChecker(checkConnectivity, useRealDelays) {
+  const zeebeAPI = new MockZeebeAPI(checkConnectivity);
+  const validator = new DeploymentPluginValidator(zeebeAPI);
+
+  const connectionChecker = validator.createConnectionChecker();
+
+  if (!useRealDelays) {
+    connectionChecker.getCheckDelay = () => 0;
+  }
+
+  return connectionChecker;
+}
+
+function MockZeebeAPI(checkConnectivity) {
+  const mockCheck = () => Promise.resolve({ success: true, response: {} });
+  this.checkConnectivity = checkConnectivity || mockCheck;
+}
